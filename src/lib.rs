da43acea7afea6e068561ee610150f788724a565
orxfun <orx.ugur.arikan@gmail.com>
2026-05-05 11:51:42 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![doc = include_str!("../README.md")]
+#![warn(
+    missing_docs,
+    clippy::unwrap_in_result,
+    clippy::unwrap_used,
+    clippy::panic,
+    clippy::panic_in_result_fn,
+    clippy::float_cmp,
+    clippy::float_cmp_const,
+    clippy::missing_panics_doc,
+    clippy::todo
+)]
+#![no_std]
+
+extern crate alloc;
