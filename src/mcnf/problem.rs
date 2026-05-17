57b0f63f07c8cb77de6ac7144b72fd0b11637c33
orxfun <orx.ugur.arikan@gmail.com>
2026-05-17 20:46:39 +0200

    add commodities to Mcnf problem

--- src/mcnf/problem.rs
+++ src/mcnf/problem.rs
@@ -1,8 +1,10 @@
-use crate::networks::Network;
+use crate::{mcnf::commodity::Commodity, networks::Network};
+use alloc::vec::Vec;
 
 pub struct Mcnf<N>
 where
     N: Network,
 {
     network: N,
+    commodities: Vec<Commodity<N>>,
 }
