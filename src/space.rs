c9d0c7ceaaff58e646e67b9a8a0537bb98df5489
orxfun <orx.ugur.arikan@gmail.com>
2026-05-21 22:28:41 +0200

    space constructor

--- src/space.rs
+++ src/space.rs
@@ -1,2 +1,8 @@
 #[derive(Clone, Copy, Debug)]
 pub struct Space(usize);
+
+impl Space {
+    pub(crate) fn new(index: usize) -> Self {
+        Self(index)
+    }
+}
--- src/time.rs
+++ src/time.rs
@@ -8,3 +8,9 @@ impl From<u64> for Time {
         Self(value)
     }
 }
+
+impl From<u32> for Time {
+    fn from(value: u32) -> Self {
+        Self(value as u64)
+    }
+}
