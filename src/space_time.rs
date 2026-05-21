3331b656d68e05435b44935f2cc4734c3bff0219
orxfun <orx.ugur.arikan@gmail.com>
2026-05-21 23:05:47 +0200

    define SpaceTime construction

--- src/space_time.rs
+++ src/space_time.rs
@@ -2,3 +2,9 @@ use crate::{space::Space, time::Time};
 
 #[derive(Clone, Copy, Debug)]
 pub struct SpaceTime(Space, Time);
+
+impl SpaceTime {
+    pub fn new(space: Space, time: impl Into<Time>) -> Self {
+        Self(space, time.into())
+    }
+}
