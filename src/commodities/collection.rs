95ed6129c5f34440f00c3254fedd118ef564bdf3
orxfun <orx.ugur.arikan@gmail.com>
2026-05-22 12:58:41 +0200

    revise commodities collection to allow for a key

--- src/commodities/collection.rs
+++ src/commodities/collection.rs
@@ -1,18 +1,27 @@
 use crate::commodities::{Commodity, CommodityData};
+use crate::indices::IndexMap;
 use crate::space_time::SpaceTime;
-use alloc::vec::Vec;
+use crate::std_utils::MapKey;
 
-#[derive(Default)]
-pub struct Commodities(Vec<CommodityData>);
+pub struct Commodities<K: MapKey> {
+    map: IndexMap<K, CommodityData, Commodity>,
+}
 
-impl Commodities {
+impl<K: MapKey> Default for Commodities<K> {
+    fn default() -> Self {
+        Self {
+            map: Default::default(),
+        }
+    }
+}
+
+impl<K: MapKey> Commodities<K> {
     pub fn new() -> Self {
         Self::default()
     }
 
-    pub fn push(&mut self, ori: SpaceTime, des: SpaceTime) -> Commodity {
-        let commodity = self.0.len().into();
-        self.0.push(CommodityData::new(ori, des));
-        commodity
+    pub fn push(&mut self, key: K, ori: SpaceTime, des: SpaceTime) -> Commodity {
+        let data = CommodityData::new(ori, des);
+        self.map.push_or_update(key, data)
     }
 }
