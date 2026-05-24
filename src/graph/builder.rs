cba96c784dac722053d4ee9d2badf8754aed2bc7
orxfun <orx.ugur.arikan@gmail.com>
2026-05-24 23:31:34 +0200

    define edge method on builder

--- src/graph/builder.rs
+++ src/graph/builder.rs
@@ -1,4 +1,4 @@
-use crate::graph::{graph::Graph, node::Node};
+use crate::graph::{edge::Edge, graph::Graph, node::Node};
 use alloc::vec::Vec;
 
 pub struct GraphBuilder<N, E>(Graph<N, E>);
@@ -9,5 +9,14 @@ impl<N, E> GraphBuilder<N, E> {
         let edges = Vec::new();
         let graph = Graph { nodes, edges };
         Self(graph)
+    }
+
+    pub fn edge(&mut self, data: E, tail: usize, head: usize) {
+        let edges_idx = self.0.edges.len();
+        let tail_out_edge_idx = self.0.nodes[tail].out_edges().len();
+        let head_in_edge_idx = self.0.nodes[head].in_edges().len();
+        self.0.edges.push(Edge::new(tail, head, data));
+        self.0.nodes[tail].add_out_edge(edges_idx, head, head_in_edge_idx);
+        self.0.nodes[head].add_in_edge(edges_idx, tail, tail_out_edge_idx);
     }
 }
--- src/graph/edge.rs
+++ src/graph/edge.rs
@@ -3,3 +3,9 @@ pub struct Edge<E> {
     head: usize,
     data: E,
 }
+
+impl<E> Edge<E> {
+    pub fn new(tail: usize, head: usize, data: E) -> Self {
+        Self { tail, head, data }
+    }
+}
