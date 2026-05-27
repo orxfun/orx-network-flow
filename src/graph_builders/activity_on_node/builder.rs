170b9bf7535800c74196935ce32e7f588fcab415
orxfun <orx.ugur.arikan@gmail.com>
2026-05-27 15:48:23 +0200

    wip build graph

--- src/graph_builders/activity_on_node/builder.rs
+++ src/graph_builders/activity_on_node/builder.rs
@@ -1,5 +1,12 @@
+use crate::graph_builders::activity_on_node::{edge::EdgeData, vertex::VertexData};
 use crate::{Graph, Problem, Variant};
 
-pub fn build<V: Variant>(problem: &Problem<V>) -> Graph<usize, usize> {
+pub fn build<V: Variant>(problem: &Problem<V>) -> Graph<VertexData, EdgeData> {
+    let num_sources = problem.len_commodities();
+    let num_destinations = problem.len_commodities();
+    let num_transports = problem.len_transports();
+
+    let v = num_sources + num_destinations + num_transports;
+
     todo!()
 }
