a7adf7fc03ae6fc94f70beda12ef7dccd090f888
orxfun <orx.ugur.arikan@gmail.com>
2026-05-31 22:35:48 +0200

    clean up edges_source_to_transport logic

--- src/graph_builders/activity_on_node/edges_source_to_transport.rs
+++ src/graph_builders/activity_on_node/edges_source_to_transport.rs
@@ -5,6 +5,7 @@ use crate::graph_builders::activity_on_node::{edge::EdgeData, vertex::VertexData
 use crate::indices::IdxCore;
 use crate::transports::Transport;
 use crate::{Problem, Variant};
+use core::iter::Peekable;
 
 pub fn edges_source_to_transport<V: Variant>(
     prob: &Problem<V>,
@@ -15,7 +16,7 @@ pub fn edges_source_to_transport<V: Variant>(
         if let Some(des_sorted_transports) = prob.ori_des_sorted_transports.get(ori) {
             for (_des, sorted_transports) in des_sorted_transports {
                 let commodities_rev = sorted_commodities.iter().rev().copied();
-                let transports_rev = sorted_transports.iter().rev().copied();
+                let transports_rev = sorted_transports.iter().rev().copied().peekable();
 
                 connect_transports_of_od(prob, builder, indexer, commodities_rev, transports_rev);
             }
@@ -28,7 +29,7 @@ fn connect_transports_of_od<V: Variant>(
     builder: &mut GraphBuilder<VertexData, EdgeData>,
     indexer: &Indexer,
     mut commodities_rev: impl Iterator<Item = Commodity>,
-    mut transports_rev: impl Iterator<Item = Transport>,
+    mut transports_rev: Peekable<impl Iterator<Item = Transport>>,
 ) -> Option<()> {
     // no edges once we complete traversing transports
     let mut curr_t = transports_rev.next()?;
@@ -38,49 +39,50 @@ fn connect_transports_of_od<V: Variant>(
         // no edges once we complete traversing commodities
         let c = commodities_rev.next()?;
 
-        let (t, next_t) = find_transport_for_commodity(prob, &mut transports_rev, curr_t, c);
+        match find_transport_for_commodity(prob, &mut transports_rev, curr_t, c) {
+            Some(t) => {
+                let data = EdgeData::SourceToTransport(c, t);
+                let tail = indexer.source_idx(c).into_inner();
+                let head = indexer.transport_idx(t).into_inner();
+                builder.edge(data, tail, head);
 
-        if let Some(t) = t {
-            let data = EdgeData::SourceToTransport(c, t);
-            let tail = indexer.source_idx(c).into_inner();
-            let head = indexer.transport_idx(t).into_inner();
-            builder.edge(data, tail, head);
+                // no point in assigning same transport to prior tails, progressing
+                curr_t = transports_rev.next()?;
+            }
+            // no transport for this commodity, moving on to the next commodity
+            None => {}
         }
-
-        // no edges once we complete traversing transports
-        curr_t = next_t?;
     }
 }
 
 fn find_transport_for_commodity<V: Variant>(
     prob: &Problem<V>,
-    transports_rev: &mut impl Iterator<Item = Transport>,
+    transports_rev: &mut Peekable<impl Iterator<Item = Transport>>,
     curr_t: Transport,
     c: Commodity,
-) -> (Option<Transport>, Option<Transport>) {
+) -> Option<Transport> {
     let ready = prob.commodity_by_idx(c).origin().time();
     let departure = prob.transport_by_idx(curr_t).origin().time();
 
     if ready > departure {
         // no transport can be connected to c
-        // but curr_t can still be connected to earlier commodities
-        return (None, Some(curr_t));
+        return None;
     }
 
     let mut curr_t = curr_t;
     loop {
-        match transports_rev.next() {
-            Some(next_t) => {
+        match transports_rev.peek() {
+            Some(&next_t) => {
                 let departure = prob.transport_by_idx(next_t).origin().time();
                 match ready <= departure {
                     // next_t can also connect to c, so it must be preferred
-                    true => curr_t = next_t,
-                    // curr_t can connect to c; next_t can be connected to earlier commodities
-                    false => return (Some(curr_t), Some(next_t)),
+                    true => curr_t = transports_rev.next().expect("is-some"),
+                    // curr_t can connect to c
+                    false => return Some(curr_t),
                 }
             }
             // curr_t is the earliest transport and can connect to c
-            None => return (Some(curr_t), None),
+            None => return Some(curr_t),
         }
     }
 }
