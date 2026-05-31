455732854d3c780e3f0dd2e92a2a2a871769627a
orxfun <orx.ugur.arikan@gmail.com>
2026-05-31 11:25:08 +0200

    implement Display for node settings

--- src/graph/visualization/dot/node_settings.rs
+++ src/graph/visualization/dot/node_settings.rs
@@ -1,9 +1,30 @@
 use crate::graph::visualization::dot::{NodeShape, node_style::NodeStyle};
 use alloc::string::String;
+use core::fmt::Display;
 
 #[derive(Default)]
 pub struct NodeSettings {
     pub shape: Option<NodeShape>,
     pub style: Option<NodeStyle>,
     pub fill_color: Option<String>,
+}
+
+impl Display for NodeSettings {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match (&self.shape, &self.style, &self.fill_color) {
+            (None, None, None) => Ok(()),
+            _ => {
+                if let Some(shape) = &self.shape {
+                    write!(f, ", shape={}", shape)?;
+                }
+                if let Some(style) = &self.style {
+                    write!(f, ", style={}", style)?;
+                }
+                if let Some(fill_color) = &self.fill_color {
+                    write!(f, ", fillcolor={}", fill_color)?;
+                }
+                Ok(())
+            }
+        }
+    }
 }
