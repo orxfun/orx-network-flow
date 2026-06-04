b26fd7d35b18441150444b82e2027bc099d449fd
orxfun <orx.ugur.arikan@gmail.com>
2026-06-04 19:35:11 +0200

    remove transport-to-transport waiting edge variant

--- src/graph_builders/activity_on_node/edge.rs
+++ src/graph_builders/activity_on_node/edge.rs
@@ -3,7 +3,6 @@ use crate::{commodities::Commodity, transports::Transport};
 pub enum EdgeData {
     SourceToSourceWait(Commodity, Commodity),
     SinkToSinkWait(Commodity, Commodity),
-    TransportToTransportWait(Transport, Transport),
     SourceToSink(Commodity),
     SourceToTransport(Commodity, Transport),
     TransportToTransport(Transport, Transport),
