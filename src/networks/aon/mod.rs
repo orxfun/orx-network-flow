d0e1e7e441f37a02156e134e9a161fa4182e3f5b
orxfun <orx.ugur.arikan@gmail.com>
2026-06-05 13:37:56 +0200

    define add_source_to_teleport_edges

--- src/networks/aon/mod.rs
+++ src/networks/aon/mod.rs
@@ -1,8 +1,12 @@
 mod edge;
 mod network;
 mod sinks;
+mod source_to_teleport;
 mod source_to_transport;
 mod sources;
+mod teleport_to_sink;
+mod transport_to_sink;
+mod transport_to_transport;
 mod vertex;
 mod visualization;
 
---
+++ src/networks/aon/source_to_teleport.rs
@@ -0,0 +1,12 @@
+use crate::networks::aon::{AonEdge, AonVertex};
+use crate::networks::aon::{sinks::Sinks, sources::Sources};
+use crate::{Problem, Variant, graph::GraphBuilder};
+
+pub fn add_source_to_teleport_edges<V: Variant>(
+    p: &Problem<V>,
+    sources: &Sources,
+    sinks: &Sinks,
+    builder: &mut GraphBuilder<AonVertex, AonEdge>,
+) {
+    todo!()
+}
