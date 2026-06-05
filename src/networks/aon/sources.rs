72ebd334d0f5deff01563cd37dcc35f2c2f9c2a2
orxfun <orx.ugur.arikan@gmail.com>
2026-06-05 16:48:45 +0200

    clean up

--- src/networks/aon/sources.rs
+++ src/networks/aon/sources.rs
@@ -1,55 +1,12 @@
 use crate::commodities::Commodity;
 use crate::indices::IdxMap;
 use crate::space_time::SpaceTime;
-use crate::spaces::Space;
-use crate::std_utils::{Map, Set};
+use crate::std_utils::Set;
 use crate::time::Time;
 use crate::{Problem, Variant, impl_idx};
 use alloc::vec::Vec;
 
 impl_idx!(SourceIdx);
-
-// pub struct Sources {
-//     idx_map: IdxMap<SpaceTime, (), SourceIdx>,
-// }
-
-// impl Sources {
-//     pub fn create<V: Variant>(p: &Problem<V>) -> Self {
-//         let mut departures = Set::default();
-
-//         for ori in p.ori_sorted_commodities.keys() {
-//             if let Some(des_transports) = p.ori_des_sorted_transports.get(ori) {
-//                 for &transport in des_transports.values().flat_map(|x| x.iter()) {
-//                     let dt = p.transport_by_idx(transport).origin().time();
-//                     let st = SpaceTime::new(*ori, dt);
-//                     departures.insert(st);
-//                 }
-//             }
-//         }
-//         let mut departures: Vec<_> = departures.into_iter().collect();
-//         departures.sort();
-
-//         let idx_map = departures.into_iter().map(|key| (key, ())).collect();
-
-//         Self { idx_map }
-//     }
-
-//     pub fn len(&self) -> usize {
-//         self.idx_map.len()
-//     }
-
-//     pub fn get_st(&self, idx: SourceIdx) -> Option<SpaceTime> {
-//         self.idx_map.idx_to_key(idx).copied()
-//     }
-
-//     pub fn get_s_idx(&self, st: SpaceTime) -> Option<SourceIdx> {
-//         self.idx_map.key_to_idx(&st)
-//     }
-
-//     pub fn iter_st_sorted(&self) -> impl Iterator<Item = SpaceTime> {
-//         self.idx_map.keys().copied()
-//     }
-// }
 
 pub struct Sources {
     idx_map: IdxMap<SpaceTime, Source, SourceIdx>,
