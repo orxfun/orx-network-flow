bf81039bff438b11c748f0a1ad8a1367ed36b24c
orxfun <orx.ugur.arikan@gmail.com>
2026-06-07 19:47:21 +0200

    add data to extended vertex

--- src/graph_extended/vertex.rs
+++ src/graph_extended/vertex.rs
@@ -2,8 +2,9 @@ use crate::VIdx;
 use crate::graph::{InEdge, OutEdge};
 use alloc::vec::Vec;
 
-pub struct ExtVertex {
+pub struct ExtVertex<Ve> {
     core_vidx: VIdx,
+    data: Ve,
     ext_out_edges: Vec<OutEdge>,
     ext_in_edges: Vec<InEdge>,
 }
