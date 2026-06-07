fae778fb4023b8045a5afcb17fb559a77e33bef1
orxfun <orx.ugur.arikan@gmail.com>
2026-06-07 19:26:08 +0200

    wip

--- src/networks/agg_ready_due/nw.rs
+++ src/networks/agg_ready_due/nw.rs
@@ -1,7 +1,7 @@
-use crate::{Graph, Problem, Variant};
+use crate::{CoreNw, Graph, Problem, Variant};
 
 #[derive(derive_new::new)]
 pub struct AggReadyDueNw<'a, V: Variant> {
     p: &'a Problem<V>,
-    // graph: Graph<CoreNwVertex, CoreNwEdge>,
+    core: &'a CoreNw<'a, V>,
 }
