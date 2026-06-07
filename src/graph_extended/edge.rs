aea91ad3820ac152a793cc8c11e9118a17c9ab2e
orxfun <orx.ugur.arikan@gmail.com>
2026-06-07 19:58:04 +0200

    define ExtEdge

--- src/graph_extended/edge.rs
+++ src/graph_extended/edge.rs
@@ -1,1 +1,18 @@
-pub struct ExtEdge {}
+use crate::graph::EIdx;
+use core::marker::PhantomData;
+
+pub struct ExtEdge<E, Ee> {
+    core_eidx: EIdx,
+    data: Ee,
+    core_data: PhantomData<E>,
+}
+
+impl<E, Ee> ExtEdge<E, Ee> {
+    pub fn new(core_eidx: EIdx, data: Ee) -> Self {
+        Self {
+            core_eidx,
+            data,
+            core_data: PhantomData,
+        }
+    }
+}
