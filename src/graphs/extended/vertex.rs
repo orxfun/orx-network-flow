98f93c42c2b00b188e0565e74bc3efbca8f2de33
orxfun <orx.ugur.arikan@gmail.com>
2026-06-08 14:02:06 +0200

    define ExtVertex

--- src/graphs/extended/vertex.rs
+++ src/graphs/extended/vertex.rs
@@ -1,4 +1,4 @@
-use crate::graphs::{InEdge, OutEdge, VIdx};
+use crate::graphs::{InEdge, OutEdge, VIdx, core::VertexCore};
 use alloc::vec::Vec;
 
 pub struct OriVertex<V> {
@@ -6,4 +6,9 @@ pub struct OriVertex<V> {
     pub(super) ext_data: V,
     pub(super) ext_out_edges: Vec<OutEdge>,
     pub(super) ext_in_edges: Vec<InEdge>,
+}
+
+pub enum ExtVertex<V> {
+    Ori(OriVertex<V>),
+    New(VertexCore<V>),
 }
