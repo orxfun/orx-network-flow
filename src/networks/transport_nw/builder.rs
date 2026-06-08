f619c32ae1936f0aa3329866680466c41179200a
orxfun <orx.ugur.arikan@gmail.com>
2026-06-08 22:20:32 +0200

    init builder for transport newtork

--- src/networks/transport_nw/builder.rs
+++ src/networks/transport_nw/builder.rs
@@ -1,6 +1,10 @@
+use crate::graphs::core::GraphCore;
 use crate::networks::transport_nw::nw::TrNw;
 use crate::{Problem, Variant};
 
 pub fn create_tr_nw<V: Variant>(p: &Problem<V>) -> TrNw<'_, V> {
-    todo!()
+    let mut builder = GraphCore::builder();
+
+    let graph = builder.finish();
+    TrNw::new(p, graph)
 }
