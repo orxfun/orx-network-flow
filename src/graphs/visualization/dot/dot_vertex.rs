6035b35d4dfaf14daf90995c6c98583fe2358a4d
orxfun <orx.ugur.arikan@gmail.com>
2026-06-08 13:15:55 +0200

    clean up

--- src/graphs/visualization/dot/dot_vertex.rs
+++
@@ -1,9 +0,0 @@
-use crate::graphs::visualization::dot::node_settings::NodeSettings;
-
-pub trait DotVertex {
-    fn label(&self) -> &str;
-
-    fn settings(&self) -> &NodeSettings;
-
-    fn tooltip(&self) -> Option<&str>;
-}
--- src/graphs/visualization/dot/mod.rs
+++ src/graphs/visualization/dot/mod.rs
@@ -1,11 +1,9 @@
 mod dot_graph;
-mod dot_vertex;
 mod node_settings;
 mod node_shape;
 mod node_style;
 
 pub use dot_graph::DotGraph;
-pub use dot_vertex::DotVertex;
 pub use node_settings::NodeSettings;
 pub use node_shape::NodeShape;
 pub use node_style::NodeStyle;
