aa7676a5bbe78412e35699b35a5afbac0c64e0a6
orxfun <orx.ugur.arikan@gmail.com>
2026-06-08 19:14:46 +0200

    impl new_v_idx

--- src/graphs/extended/graph_extended.rs
+++ src/graphs/extended/graph_extended.rs
@@ -1,6 +1,7 @@
 use crate::graphs::core::{EdgeCore, VertexCore};
 use crate::graphs::extended::{edge::OriEdge, vertex::OriVertex};
-use crate::graphs::{Graph, VecEdge, VecVertex};
+use crate::graphs::{Graph, VIdx, VecEdge, VecVertex};
+use crate::indices::IdxCore;
 
 pub struct GraphExtended<'a, G, Dv, De>
 where
@@ -11,4 +12,17 @@ where
     pub(super) core_edges: VecEdge<OriEdge<'a, G::E<'a>, De>>,
     pub(super) new_vertices: VecVertex<VertexCore<Dv>>,
     pub(super) new_edges: VecEdge<EdgeCore<De>>,
+}
+
+impl<'a, G, Dv, De> GraphExtended<'a, G, Dv, De>
+where
+    G: Graph,
+{
+    pub(super) fn new_v_idx(&self, v: VIdx) -> Option<VIdx> {
+        let v = v.into_inner();
+        match v < self.core.v() {
+            true => None,
+            false => Some(VIdx::from(v - self.core.v())),
+        }
+    }
 }
