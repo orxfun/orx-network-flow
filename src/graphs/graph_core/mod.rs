0c20b3f2944aa8207cee6779fb05ed72d68637bc
orxfun <orx.ugur.arikan@gmail.com>
2026-06-08 10:02:04 +0200

    clean up

--- src/graphs/graph_core/mod.rs
+++ src/graphs/graph_core/mod.rs
@@ -3,16 +3,8 @@ mod tests;
 
 mod builder;
 mod display;
-// mod edge;
 mod graph;
-// mod in_edge;
-// mod out_edge;
-// mod vertex;
 pub mod visualization;
 
 pub use builder::GraphBuilder;
-// pub use edge::{EIdx, Edge, VecEdge};
 pub use graph::GraphCore;
-// pub use in_edge::InEdge;
-// pub use out_edge::OutEdge;
-// pub use vertex::{VIdx, VecVertex, Vertex};
