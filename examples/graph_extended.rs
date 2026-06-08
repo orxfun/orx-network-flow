5122b40f97aa8100031c388e00c2719233c6e6b8
orxfun <orx.ugur.arikan@gmail.com>
2026-06-08 21:52:17 +0200

    example for recursive extension

--- examples/graph_extended.rs
+++ examples/graph_extended.rs
@@ -16,6 +16,8 @@ fn main() {
 
     let core = builder.finish();
 
+    // extend core graph
+
     let core_vertices = core.vertex_indices().map(|v: VIdx| v.to_string());
     let core_edges = core
         .edges()
@@ -33,6 +35,25 @@ fn main() {
 
     let dot = extended.as_dot_graph();
     dot.create_svg_file("target/graph_extended.dot", "target/graph_extended.svg")
+        .unwrap();
+
+    // recursive extension - extend extended graph
+
+    let core_vertices = extended.vertex_indices().map(|v: VIdx| v.to_string());
+    let core_edges = extended
+        .edges()
+        .map(|e| e.head().into_inner() + e.tail().into_inner());
+    let mut builder =
+        GraphExtended::<_, String, usize>::builder(&extended, core_vertices, core_edges);
+
+    let v6 = builder.node("z".to_string());
+    builder.edge(99, v4, v6);
+    builder.edge(99, v5, v6);
+
+    let extended2 = builder.finish();
+
+    let dot = extended2.as_dot_graph();
+    dot.create_svg_file("target/graph_extended2.dot", "target/graph_extended2.svg")
         .unwrap();
 
     println!("\n\nDOT\n{}", dot.dot_string());
