d767dc5912350fc6aca6533eadb3de9c5f5ab92c
orxfun <orx.ugur.arikan@gmail.com>
2026-06-08 09:56:41 +0200

    define common graph types

--- src/graphs/mod.rs
+++ src/graphs/mod.rs
@@ -1,5 +1,9 @@
+mod edge;
 pub mod graph_core;
 mod graph_extended;
+mod in_edge;
+mod out_edge;
+mod vertex;
 
 pub use graph_core::{
     EIdx, Edge, GraphBuilder, GraphCore, InEdge, OutEdge, VIdx, VecEdge, VecVertex, Vertex,
---
+++ src/graphs/out_edge.rs
@@ -0,0 +1,29 @@
+use crate::graphs::{edge::EIdx, vertex::VIdx};
+
+pub struct OutEdge {
+    edges_idx: EIdx,
+    head: VIdx,
+    head_in_edge_pos: usize,
+}
+
+impl OutEdge {
+    pub fn new(edges_idx: EIdx, head: VIdx, head_in_edge_pos: usize) -> Self {
+        Self {
+            edges_idx,
+            head,
+            head_in_edge_pos,
+        }
+    }
+
+    pub fn edges_idx(&self) -> EIdx {
+        self.edges_idx
+    }
+
+    pub fn head(&self) -> VIdx {
+        self.head
+    }
+
+    pub fn head_in_edge_idx(&self) -> usize {
+        self.head_in_edge_pos
+    }
+}
