c5e9722155a69f5f5aad94c67d58bc27af2de9fb
orxfun <orx.ugur.arikan@gmail.com>
2026-06-08 18:29:40 +0200

    simplify in and out edges

--- src/graphs/extended/vertex.rs
+++ src/graphs/extended/vertex.rs
@@ -1,11 +1,11 @@
-use crate::graphs::{EIdx, InEdge, OutEdge, VIdx, Vertex, core::VertexCore};
+use crate::graphs::{EIdx, VIdx, Vertex, core::VertexCore};
 use alloc::vec::Vec;
 
 pub struct OriVertex<V> {
     pub(super) core_v: VIdx,
     pub(super) ext_data: V,
-    pub(super) ext_out_edges: Vec<OutEdge>,
-    pub(super) ext_in_edges: Vec<InEdge>,
+    pub(super) ext_out_edges: Vec<EIdx>,
+    pub(super) ext_in_edges: Vec<EIdx>,
 }
 
 pub enum ExtVertex<V> {
--- src/graphs/mod.rs
+++ src/graphs/mod.rs
@@ -4,8 +4,6 @@ pub mod extended;
 mod graph;
 mod graph_mut;
 // mod graph_extended;
-mod in_edge;
-mod out_edge;
 mod vertex;
 pub mod visualization;
 
@@ -13,6 +11,3 @@ pub use core::{GraphBuilderCore, GraphCore};
 pub use edge::{EIdx, Edge, VecEdge};
 pub use graph::Graph;
 pub use vertex::{VIdx, VecVertex, Vertex};
-
-pub(super) use in_edge::InEdge;
-pub(super) use out_edge::OutEdge;
