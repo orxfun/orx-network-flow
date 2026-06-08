b8cdc91c3374b3733ac13a04283104c227da4f0f
orxfun <orx.ugur.arikan@gmail.com>
2026-06-08 10:21:38 +0200

    define Edge trait

--- src/graphs/edge.rs
+++ src/graphs/edge.rs
@@ -1,4 +1,14 @@
-use crate::{impl_idx, impl_vec_of_idx};
+use crate::{graphs::VIdx, impl_idx, impl_vec_of_idx};
 
 impl_idx!(EIdx);
 impl_vec_of_idx!(EIdx, VecEdge);
+
+pub trait Edge {
+    type Data;
+
+    fn data(&self) -> &Self::Data;
+
+    fn tail(&self) -> VIdx;
+
+    fn head(&self) -> VIdx;
+}
