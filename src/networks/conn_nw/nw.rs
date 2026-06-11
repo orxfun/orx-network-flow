0a98bbbff3c90e9837413c82d86a296d58c3dd4a
orxfun <orx.ugur.arikan@gmail.com>
2026-06-11 09:40:39 +0200

    wire up dot graph with conn nw

--- src/networks/conn_nw/nw.rs
+++ src/networks/conn_nw/nw.rs
@@ -1,6 +1,21 @@
-use crate::{Problem, Variant, graphs::core::GraphCore};
+use crate::graphs::{core::GraphCore, visualization::dot::NodeSettings};
+use crate::networks::conn_nw::visualization::dot::DotConnNw;
+use crate::{Problem, Variant};
 
 pub struct ConnNw<'a, V: Variant> {
     pub(super) p: &'a Problem<V>,
     pub(super) g: GraphCore<(), ()>,
+}
+
+impl<'a, V> ConnNw<'a, V>
+where
+    V: Variant,
+{
+    pub fn construct(p: &'a Problem<V>) -> Self {
+        todo!()
+    }
+
+    pub fn as_dot_graph(&'a self, transport_settings: Option<NodeSettings>) -> DotConnNw<'a, V> {
+        DotConnNw::new(self, transport_settings)
+    }
 }
