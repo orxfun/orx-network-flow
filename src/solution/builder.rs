8a78669ccfeb2103725de3b737c5eaaf2ed0fd8d
orxfun <orx.ugur.arikan@gmail.com>
2026-06-13 21:16:15 +0200

    builder constructor from solution

--- src/solution/builder.rs
+++ src/solution/builder.rs
@@ -7,4 +7,8 @@ impl<V: Variant> SolutionBuilder<V> {
         let commodity_flows = (0..len_commodities).map(|_| Default::default()).collect();
         Self(Solution::new(commodity_flows))
     }
+
+    pub fn finish(self) -> Solution<V> {
+        self.0
+    }
 }
--- src/solution/soln.rs
+++ src/solution/soln.rs
@@ -1,6 +1,13 @@
-use crate::{Variant, commodities::VecCommodity, solution::CommodityFlows};
+use crate::solution::{CommodityFlows, SolutionBuilder};
+use crate::{Variant, commodities::VecCommodity};
 
 #[derive(derive_new::new)]
 pub struct Solution<V: Variant> {
     commodity_flows: VecCommodity<CommodityFlows<V>>,
+}
+
+impl<V: Variant> Solution<V> {
+    pub fn builder(len_commodities: usize) -> SolutionBuilder<V> {
+        SolutionBuilder::new(len_commodities)
+    }
 }
