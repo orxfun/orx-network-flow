2eed9e2c79c3b5feee44f81c65134ddd3bdeb1b8
orxfun <orx.ugur.arikan@gmail.com>
2026-06-13 22:12:12 +0200

    map graph to shortest path

--- src/networks/conn_wait_nw/output.rs
+++ src/networks/conn_wait_nw/output.rs
@@ -10,7 +10,7 @@ pub struct Output<V: Variant> {
 
 impl<V: Variant> Output<V> {
     pub fn create(nw: &ConnWaitNw<'_, V>, edge_flows: VecEdge<V::F>) -> Self {
-        let solution = create_solution(nw, edge_flows.clone());
+        let solution = create_solution(nw, &edge_flows);
         Self {
             edge_flows,
             solution,
@@ -18,25 +18,24 @@ impl<V: Variant> Output<V> {
     }
 }
 
-fn create_solution<V: Variant>(
-    nw: &ConnWaitNw<'_, V>,
-    mut edge_flows: VecEdge<V::F>,
-) -> Solution<V> {
+fn create_solution<V: Variant>(nw: &ConnWaitNw<'_, V>, edge_flows: &VecEdge<V::F>) -> Solution<V> {
     let mut builder = Solution::builder(12);
     let b = &mut builder;
     let (p, g) = (nw.p, &nw.g);
 
     let tail = |e: EIdx| g.vertex(g.edge(e).tail());
     let tail_t = |e: EIdx| p.transport_by_idx(tail(e).data().get_t().expect("t"));
-    let edge_cost = |e: EIdx, data: &ConnWaitEdge| match data {
+    let edge_cost = |e: EIdx, x: &ConnWaitEdge| match x {
         ConnWaitEdge::Connect | ConnWaitEdge::Exit => tail_t(e).duration(),
         _ => Time::zero(),
     };
-    let edge_flow = |e: EIdx, data: &ConnWaitEdge| match data {
+    let edge_flow = |e: EIdx, x: &ConnWaitEdge| match x {
         ConnWaitEdge::Bypass(_) => Default::default(),
         _ => edge_flows[e],
     };
-    // let map_edge =
+    let map_edge = |e: EIdx, x: &ConnWaitEdge| (edge_cost(e, x), edge_flow(e, x));
+
+    let g = g.map(|_, _| (), map_edge);
 
     builder.finish()
 }
