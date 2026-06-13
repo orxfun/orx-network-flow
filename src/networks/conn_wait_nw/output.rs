f2411d29534f62fe76d83f1af41282518bc01151
orxfun <orx.ugur.arikan@gmail.com>
2026-06-13 21:22:54 +0200

    set up solution creation for output

--- src/networks/conn_wait_nw/output.rs
+++ src/networks/conn_wait_nw/output.rs
@@ -1,6 +1,24 @@
-use crate::{Solution, Variant, graphs::VecEdge};
+use crate::graphs::VecEdge;
+use crate::networks::ConnWaitNw;
+use crate::{Solution, Variant};
 
 pub struct Output<V: Variant> {
     edge_flows: VecEdge<V::F>,
     solution: Solution<V>,
+}
+
+impl<V: Variant> Output<V> {
+    pub fn create(edge_flows: VecEdge<V::F>) -> Self {
+        todo!()
+    }
+}
+
+fn create_solution<V: Variant>(
+    nw: &ConnWaitNw<'_, V>,
+    mut edge_flows: VecEdge<V::F>,
+) -> Solution<V> {
+    let mut builder = Solution::builder(12);
+    let b = &mut builder;
+
+    builder.finish()
 }
