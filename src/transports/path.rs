78048321e753ebbfdacf6db463ed62505dd321fd
orxfun <orx.ugur.arikan@gmail.com>
2026-06-13 20:39:51 +0200

    avoid allocation for small paths

--- src/transports/path.rs
+++ src/transports/path.rs
@@ -1,4 +1,9 @@
 use crate::Transport;
 use alloc::vec::Vec;
 
-pub struct Path(Vec<Transport>);
+pub enum Path {
+    OneLeg(Transport),
+    TwoLegs([Transport; 2]),
+    ThreeLegs([Transport; 3]),
+    Long(Vec<Transport>),
+}
