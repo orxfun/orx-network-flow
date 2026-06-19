9221aaedb73f5882335d206537569377a4176967
orxfun <orx.ugur.arikan@gmail.com>
2026-06-19 15:18:37 +0200

    define solve method for ro mcnf

--- src/mcnf/mcnf_solvers/edge_wait_ro_solver/solver.rs
+++ src/mcnf/mcnf_solvers/edge_wait_ro_solver/solver.rs
@@ -1,3 +1,5 @@
+use std::dbg;
+
 use crate::Variant;
 use crate::mcnf::mcnf_solvers::edge_wait_ro_solver::capacity::add_capacity_constraints;
 use crate::mcnf::mcnf_solvers::edge_wait_ro_solver::flow_balance::add_flow_balance_constraints;
@@ -5,7 +7,7 @@ use crate::mcnf::mcnf_solvers::edge_wait_ro_solver::obj::objective;
 use crate::mcnf::mcnf_solvers::edge_wait_ro_solver::params::EdgeWaitRoMcnfParams;
 use crate::mcnf::mcnf_solvers::edge_wait_ro_solver::vars::define_vars;
 use crate::networks::ConnWaitNw;
-use good_lp::Solver;
+use good_lp::{Solver, SolverModel};
 
 pub struct EdgeWaitRoMcnfSolver<'a, V: Variant, S: Solver> {
     nw: &'a ConnWaitNw<'a, V>,
@@ -38,5 +40,11 @@ impl<'a, V: Variant, S: Solver> EdgeWaitRoMcnfSolver<'a, V, S> {
     pub fn export_lp(&self, lp_path: impl AsRef<std::path::Path>) -> Result<(), std::io::Error> {
         use crate::utils::math_model::lp_solvers_model_to_lp_file;
         unsafe { lp_solvers_model_to_lp_file::<S, _>(&self.model, lp_path) }
+    }
+
+    pub fn solve(self) {
+        let solution = self.model.solve();
+        let a = solution.unwrap();
+        let b = a;
     }
 }
