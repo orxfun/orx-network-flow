81f5cbccde559a50ef7f02cffadc18ac910ab789
orxfun <orx.ugur.arikan@gmail.com>
2026-06-19 18:49:00 +0200

    wip disaggregation

--- src/mcnf/mcnf_solvers/edge_wait_ro_solver/sol.rs
+++ src/mcnf/mcnf_solvers/edge_wait_ro_solver/sol.rs
@@ -1,6 +1,7 @@
+use crate::graphs::EIdx;
 use crate::mcnf::mcnf_solvers::edge_wait_ro_solver::vars::RoVars;
 use crate::mcnf::solution::CommodityLoad;
-use crate::{FlowUnit, IdxCore, McnfSolution, Variant, VecTransport};
+use crate::{FlowUnit, McnfSolution, Variant, VecTransport};
 use crate::{commodities::VecCommodity, networks::ConnWaitNw};
 use alloc::vec::Vec;
 use good_lp::{Solution, Solver, SolverModel, Variable};
@@ -11,7 +12,7 @@ pub fn create_solution<V: Variant, S: Solver>(
     solution: &<S::Model as SolverModel>::Solution,
 ) -> McnfSolution<V> {
     let p = nw.p();
-    let var_to_flow = |x: &Variable| <V::F as FlowUnit>::from_f64(solution.value(*x));
+    let var_to_flow = |x: &Variable| FlowUnit::from_f64(solution.value(*x));
 
     let mut transport_loads: VecTransport<Vec<CommodityLoad<V>>> =
         VecTransport::new_filled(p.len_transports(), Default::default);
@@ -19,39 +20,41 @@ pub fn create_solution<V: Variant, S: Solver>(
     let bypass_edge_by_commodity = nw.bypass_edge_by_commodity();
 
     for (ro, vars) in ro_vars.iter() {
+        let edge_flow = |e: EIdx| var_to_flow(&vars[e]);
+
         let commodities = p.sorted_ro_commodities.value_by_key_unc(&ro);
 
         let mut remaining_by_commodity = Vec::with_capacity(commodities.len());
-        let mut total_remaining = 0.0;
+        let mut total_remaining = <V::F as FlowUnit>::zero();
         for &c in commodities {
             let amount = p.commodity_by_idx(c).amount();
             let bypass = bypass_edge_by_commodity[c]
-                .map(|e| var_to_flow(&vars[e]))
+                .map(edge_flow)
                 .unwrap_or_default();
-            let remaining = (amount - bypass).into_f64().max(0.0);
+            let remaining = amount - bypass;
 
-            if remaining > 0.0 {
+            if remaining.is_pos() {
                 total_remaining += remaining;
                 remaining_by_commodity.push((c, remaining));
             }
         }
 
-        if total_remaining <= 0.0 {
+        if total_remaining.is_nonpos() {
             continue;
         }
 
         for (t, edges) in nw.transport_edges() {
-            let total_load_on_transport =
-                edges.iter().map(|&e| solution.value(vars[e])).sum::<f64>();
+            let total_load_on_transport = FlowUnit::sum(edges.iter().copied().map(edge_flow));
+            // edges.iter().map(|&e| solution.value(vars[e])).sum::<f64>();
 
-            if total_load_on_transport <= 0.0 {
+            if total_load_on_transport.is_nonpos() {
                 continue;
             }
 
             let loads = &mut transport_loads[t];
             for (commodity, remaining) in &remaining_by_commodity {
-                let ratio = *remaining / total_remaining;
-                let load = <V::F as FlowUnit>::from_f64(total_load_on_transport * ratio);
+                let ratio: V::F = *remaining / total_remaining;
+                let load = total_load_on_transport * ratio;
                 loads.push(CommodityLoad {
                     commodity: *commodity,
                     load,
