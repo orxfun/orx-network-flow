365c3682eb532de90b093d72387e550a25b56135
orxfun <orx.ugur.arikan@gmail.com>
2026-06-22 18:30:25 +0200

    revise example inputs

--- examples/space_time_nw.rs
+++ examples/space_time_nw.rs
@@ -113,9 +113,9 @@ fn sample_problem() -> orx_network_flow::Problem<MyVariant> {
     };
 
     commodity("AMS", "BRU", 0, 20);
-    commodity("AMS", "CVG", 0, 20);
-    commodity("AMS", "LEJ", 0, 20);
-    commodity("AMS", "LEJ", 0, 20);
+    // commodity("AMS", "CVG", 0, 20);
+    // commodity("AMS", "LEJ", 0, 20);
+    // commodity("AMS", "LEJ", 0, 20);
     commodity("LEJ", "CVG", 0, 20);
 
     // transports
@@ -136,18 +136,19 @@ fn sample_problem() -> orx_network_flow::Problem<MyVariant> {
     };
 
     transport("AMS", "BRU", 1, 2, 10);
-    transport("AMS", "BRU", 4, 5, 10);
-    transport("AMS", "LEJ", 4, 5, 10);
-    transport("LEJ", "BRU", 1, 2, 10);
-    transport("LEJ", "BRU", 4, 5, 10);
-    transport("BRU", "CVG", 7, 12, 10);
+    transport("LEJ", "CVG", 1, 2, 10);
+    // transport("AMS", "BRU", 4, 5, 10);
+    // transport("AMS", "LEJ", 4, 5, 10);
+    // transport("LEJ", "BRU", 1, 2, 10);
+    // transport("LEJ", "BRU", 4, 5, 10);
+    // transport("BRU", "CVG", 7, 12, 10);
 
     let mut lost_revenue_cost = builder.lost_revenue_cost();
     lost_revenue_cost.commodity_specific(&0, 1);
     lost_revenue_cost.commodity_specific(&1, 3);
-    lost_revenue_cost.commodity_specific(&2, 10);
-    lost_revenue_cost.commodity_specific(&3, 2);
-    lost_revenue_cost.commodity_specific(&4, 8);
+    // lost_revenue_cost.commodity_specific(&2, 10);
+    // lost_revenue_cost.commodity_specific(&3, 2);
+    // lost_revenue_cost.commodity_specific(&4, 8);
 
     builder.finish()
 }
