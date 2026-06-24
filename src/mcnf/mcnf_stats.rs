fc15c19f759b9250a09b84b68791cfdb4581a6fb
orxfun <orx.ugur.arikan@gmail.com>
2026-06-24 15:37:32 +0200

    pub fields of mcnf stats

--- src/mcnf/mcnf_stats.rs
+++ src/mcnf/mcnf_stats.rs
@@ -1,7 +1,7 @@
 use crate::GraphStats;
 
 pub struct McnfStats {
-    graph_stats: GraphStats,
-    num_variables: usize,
-    num_constraints: usize,
+    pub graph_stats: GraphStats,
+    pub num_variables: usize,
+    pub num_constraints: usize,
 }
