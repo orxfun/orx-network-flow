a5eab3ff6bff6a64739ea7577cf4748ea8cd31a9
orxfun <orx.ugur.arikan@gmail.com>
2026-06-24 13:07:12 +0200

    preserve SortedKeyMap immutability

--- src/common_ds/sorted_key_map.rs
+++ src/common_ds/sorted_key_map.rs
@@ -53,26 +53,6 @@ where
     pub fn get(&self, key: &K) -> Option<&V> {
         self.map.get(key)
     }
-
-    #[inline]
-    pub fn get_or_add_default_mut(&mut self, key: K) -> &mut V
-    where
-        V: Default,
-    {
-        if !self.map.contains_key(&key) {
-            self.sorted_keys.push(key.clone());
-        }
-        self.map.entry(key).or_default()
-    }
-
-    pub fn values_mut(&mut self) -> impl Iterator<Item = &mut V> {
-        self.map.values_mut()
-    }
-
-    // TODO: this is not nice as the struct is not always in sorted state, move to builder pattern
-    pub fn preserve_key_order(&mut self) {
-        self.sorted_keys.sort();
-    }
 }
 
 pub struct SortedKeyMapBuilder<K, V>
