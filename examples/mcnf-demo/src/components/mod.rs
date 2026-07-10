a4cc8cdf7e84f91b82b4ba2deeaee673f271a5c7
orxfun <orx.ugur.arikan@gmail.com>
2026-07-10 09:04:43 +0200

    wip demo

--- examples/mcnf-demo/src/components/mod.rs
+++ examples/mcnf-demo/src/components/mod.rs
@@ -1,10 +1,10 @@
+mod form_problem;
 mod input_table;
 mod network_selector;
-mod problem_form;
 mod stats_panel;
 
+pub use form_problem::ProblemForm;
 pub use input_table::InputTable;
 pub use input_table::{InputCell, InputRow, RemoveItemFn};
 pub use network_selector::NetworkSelector;
-pub use problem_form::ProblemForm;
 pub use stats_panel::StatsPanel;
