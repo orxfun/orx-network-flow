b33c3e312300ad30a47af2015a8af634df5eea7b
orxfun <orx.ugur.arikan@gmail.com>
2026-07-11 23:23:44 +0200

    define space kind

--- examples/demos/time_expansion/src/pages/problem/spaces.rs
+++ examples/demos/time_expansion/src/pages/problem/spaces.rs
@@ -1,6 +1,12 @@
 use crate::app_data::AppData;
 use leptos::*;
 
+pub enum SpaceKind {
+    Basic,
+    Euclidean,
+    Geographic,
+}
+
 #[component]
 pub fn ViewSpaces() -> impl IntoView {
     let app = expect_context::<AppData>();
