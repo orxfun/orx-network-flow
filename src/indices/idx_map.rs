80ec579f8b2adbbbb7ffc4dc79ea07936a738f01
orxfun <orx.ugur.arikan@gmail.com>
2026-08-16 18:10:53 +0200

    minor renaming

--- src/indices/idx_map.rs
+++ src/indices/idx_map.rs
@@ -5,16 +5,16 @@ use core::fmt::Debug;
 use core::marker::PhantomData;
 
 pub struct IdxMap<K: MapKey, V, I: Idx> {
-    index_and_data: Vec<(K, V)>,
-    key_to_index: Map<K, usize>,
+    key_and_data: Vec<(K, V)>,
+    key_to_pos: Map<K, usize>,
     p: PhantomData<fn() -> I>,
 }
 
 impl<K: MapKey + Debug, V: Debug, I: Idx> Debug for IdxMap<K, V, I> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         f.debug_struct("IdxMap")
-            .field("index_and_data", &self.index_and_data)
-            .field("key_to_index", &self.key_to_index)
+            .field("index_and_data", &self.key_and_data)
+            .field("key_to_index", &self.key_to_pos)
             .finish()
     }
 }
@@ -22,8 +22,8 @@ impl<K: MapKey + Debug, V: Debug, I: Idx> Debug for IdxMap<K, V, I> {
 impl<K: MapKey, V, I: Idx> Default for IdxMap<K, V, I> {
     fn default() -> Self {
         Self {
-            index_and_data: Default::default(),
-            key_to_index: Default::default(),
+            key_and_data: Default::default(),
+            key_to_pos: Default::default(),
             p: Default::default(),
         }
     }
@@ -47,36 +47,33 @@ impl<K: MapKey, V, I: Idx> Extend<(K, V)> for IdxMap<K, V, I> {
 
 impl<K: MapKey, V, I: Idx> IdxMap<K, V, I> {
     pub fn push_or_update(&mut self, key: K, data: V) -> I {
-        match self.key_to_index.get(&key) {
+        match self.key_to_pos.get(&key) {
             Some(&pos) => {
-                debug_assert_eq!(&self.index_and_data[pos].0, &key);
-                self.index_and_data[pos].1 = data;
+                debug_assert_eq!(&self.key_and_data[pos].0, &key);
+                self.key_and_data[pos].1 = data;
                 pos.into()
             }
             None => {
-                let pos = self.index_and_data.len();
-                self.index_and_data.push((key.clone(), data));
-                self.key_to_index.insert(key, pos);
+                let pos = self.key_and_data.len();
+                self.key_and_data.push((key.clone(), data));
+                self.key_to_pos.insert(key, pos);
                 pos.into()
             }
         }
     }
 
     pub fn len(&self) -> usize {
-        self.index_and_data.len()
+        self.key_and_data.len()
     }
 
     pub fn value_by_key(&self, key: &K) -> Option<&V> {
-        let pos = *self.key_to_index.get(key)?;
-        Some(&self.index_and_data[pos].1)
+        let pos = *self.key_to_pos.get(key)?;
+        Some(&self.key_and_data[pos].1)
     }
 
     pub fn value_by_key_unc(&self, key: &K) -> &V {
-        let pos = *self
-            .key_to_index
-            .get(key)
-            .expect("missing key in index map");
-        Some(&self.index_and_data[pos].1).expect("missing key in index map")
+        let pos = *self.key_to_pos.get(key).expect("missing key in index map");
+        Some(&self.key_and_data[pos].1).expect("missing key in index map")
     }
 
     pub fn value(&self, idx: I) -> Option<&V>
@@ -84,7 +81,7 @@ impl<K: MapKey, V, I: Idx> IdxMap<K, V, I> {
         I: IdxCore,
     {
         let idx = idx.into_inner();
-        self.index_and_data.get(idx).map(|x| &x.1)
+        self.key_and_data.get(idx).map(|x| &x.1)
     }
 
     pub fn value_unchecked(&self, idx: I) -> &V
@@ -92,7 +89,7 @@ impl<K: MapKey, V, I: Idx> IdxMap<K, V, I> {
         I: IdxCore,
     {
         let idx = idx.into_inner();
-        &self.index_and_data[idx].1
+        &self.key_and_data[idx].1
     }
 
     pub fn key(&self, idx: I) -> Option<&K>
@@ -100,23 +97,23 @@ impl<K: MapKey, V, I: Idx> IdxMap<K, V, I> {
         I: IdxCore,
     {
         let idx = idx.into_inner();
-        self.index_and_data.get(idx).map(|x| &x.0)
+        self.key_and_data.get(idx).map(|x| &x.0)
     }
 
     pub fn key_to_idx(&self, key: &K) -> Option<I> {
-        let pos = *self.key_to_index.get(key)?;
+        let pos = *self.key_to_pos.get(key)?;
         Some(I::from(pos))
     }
 
     pub fn entries(&self) -> impl Iterator<Item = (I, &K, &V)> {
-        self.index_and_data
+        self.key_and_data
             .iter()
             .enumerate()
             .map(|(pos, (key, data))| (I::from(pos), key, data))
     }
 
     pub fn keys(&self) -> impl Iterator<Item = &K> {
-        self.index_and_data.iter().map(|x| &x.0)
+        self.key_and_data.iter().map(|x| &x.0)
     }
 
     pub fn indices(&self) -> impl Iterator<Item = I> {
@@ -124,24 +121,24 @@ impl<K: MapKey, V, I: Idx> IdxMap<K, V, I> {
     }
 
     pub fn keys_indices(&self) -> impl Iterator<Item = (&K, I)> {
-        self.index_and_data
+        self.key_and_data
             .iter()
             .enumerate()
             .map(|(i, x)| (&x.0, i.into()))
     }
 
     pub fn values(&self) -> impl Iterator<Item = &V> {
-        self.index_and_data.iter().map(|x| &x.1)
+        self.key_and_data.iter().map(|x| &x.1)
     }
 
     pub fn indices_values(&self) -> impl Iterator<Item = (I, &V)> {
-        self.index_and_data
+        self.key_and_data
             .iter()
             .enumerate()
             .map(|(i, x)| (I::from(i), &x.1))
     }
 
     pub fn index_and_data(&self) -> &[(K, V)] {
-        &self.index_and_data
+        &self.key_and_data
     }
 }
